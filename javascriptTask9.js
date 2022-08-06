03bc6e4f400b62aa983642999b5a20bef7059db9
SudhaGurung-cedcoss <sudhakumarigurung@cedcoss.com>
2022-08-06 11:53:41 +0530

    Task 9 modified

---
+++ javascriptTask9.js
@@ -0,0 +1,40 @@
+
+
+
+ 
+function add(){
+
+var id=document.getElementById('id').value;
+ var Name=document.getElementById('name').value;
+ var basicSalary=document.getElementById('basicSalary').value;
+
+var username=/^[a-z A-Z]+$/;
+var number=/^[0-9]+$/;
+    try{
+        if(id =="" || basicSalary=="")
+        {
+            throw "ID cannot be empty";         
+        }
+        else{
+            document.getElementById('span').innerHTML="";
+        }
+    }
+    catch(err) {
+        document.getElementById('span').innerHTML="all Field must be filled";
+     
+    }
+    
+    try{
+        if(Name.match(username)){
+          document.getElementById('nameError').innerHTML="";
+        }
+       else{
+         throw "invalid expression"
+       }
+     }
+     catch(err){
+     document.getElementById('nameError').innerHTML=err;
+     document.getElementById('nameError').style.color=red;
+    } 
+   
+}
